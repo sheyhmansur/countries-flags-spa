43eb16a65f792c2b8efea9af3ffb0cabe65972f6
Sheyh-Mansur Dzhavathanov <92082169+sheyhmansur@users.noreply.github.com>
2021-12-18 11:54:19 +0300

    added components container.jsx and header.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  return <div className="App">dsd</div>;
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import { IoMoon, IoMoonOutline } from "react-icons/io5";
+
+import { Container } from "./Container";
+
+export const Header = () => {
+  return (
+    <div>
+      <Container />
+    </div>
+  );
+};
+
+export default Header;
