a98b434ecbdb6d667745f264c56b2e6fe71b8075
Sheyh-Mansur Dzhavathanov <92082169+sheyhmansur@users.noreply.github.com>
2021-12-24 19:23:40 +0300

    added List.jsx and Card.jsx

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
 const BASE_URL = "https://restcountries.com/v2/";
 
 export const ALL_COUNTRIES = BASE_URL + "all";
+//будет принимать имя страны
+export const searchByCountry = (name) => BASE_URL + "name/" + name; // - name - тот name который придет в эту функцию
+// сформирую конкретную ссылку, сделаю на нее запрос, получу одну конкретную страну, буду с ней работать
 
-export const searchByCountry = (name) => BASE_URL + "/name/" + name;
+// будем получать некий набор кодов
+export const filterByCode = (codes) => BASE_URL + "alpha?codes=" + codes.join();
