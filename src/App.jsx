9be54a230c397f3cece87225caf0eeac3969b425
Sheyh-Mansur Dzhavathanov <92082169+sheyhmansur@users.noreply.github.com>
2021-12-29 18:38:08 +0300

    created routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from "react";
 import { Controls } from "./components/Controls";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
+import { List } from "./components/List";
+import { Card } from "./components/Card";
 import { ALL_COUNTRIES } from "./config";
 
 function App() {
@@ -21,6 +23,30 @@ function App() {
       <Header />
       <Main>
         <Controls />
+        <List>
+          {countries.map((c) => {
+            const countryInfo = {
+              img: c.flags.png,
+              name: c.name,
+              info: [
+                {
+                  title: "Population",
+                  description: c.population.toLocaleString(),
+                },
+                {
+                  title: "Region",
+                  description: c.region,
+                },
+                {
+                  title: "Capital",
+                  description: c.capital,
+                },
+              ],
+            };
+
+            return <Card key={c.name} {...countryInfo} />;
+          })}
+        </List>
       </Main>
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,52 @@
 import styled from "styled-components";
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+  border-radius: var(--radii);
+  background-color: var(--colors-ui-base);
+  box-shadow: var(--shadow);
+  cursor: pointer;
+  overflow: hidden;
+`;
 
-const CardImage = styled.img``;
+const CardImage = styled.img`
+  display: block;
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+  object-position: center;
+  box-shadow: var(--shadow);
+`;
 
-const CardBody = styled.div``;
+const CardBody = styled.div`
+  padding: 1rem 1.5rem 2rem;
+`;
 
-const CardTitle = styled.h3``;
+const CardTitle = styled.h3`
+  margin: 0;
+  font-size: var(--fs-md);
+  font-weight: var(--fw-bold);
+`;
 
-const CardList = styled.ul``;
+const CardList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 1rem 0 0;
+`;
 
-const CardListItem = styled.li``;
+const CardListItem = styled.li`
+  font-size: var(--fs-sm);
+  line-height: 1.5;
+  font-weight: var(--fw-light);
 
-export const Card = (img, name, info = [], onClick) => {
+  & > b {
+    font-weight: var(--fw-bold);
+  }
+`;
+
+export const Card = ({ img, name, info = [], onClick }) => {
   return (
     <Wrapper OnClick={onClick}>
-      <CardImage />
+      <CardImage src={img} alt={name} />
       <CardBody>
         <CardTitle>{name}</CardTitle>
         <CardList>
